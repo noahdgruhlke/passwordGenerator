cadef6073c4be4976ab3a475ca31b8a37a68d6c1
Noah Gruhlke <noahdgruhlke@gmail.com>
2024-10-07 09:14:51 -0600

    edited the applications 'main' injection parameter

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+class PasswordGenerator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.ReadLine();
+
+        return;
+    }
+}
