b74b6f9329a70635b7d639f4663c731f906e744f
Noah Gruhlke <noahdgruhlke@gmail.com>
2024-10-23 09:41:33 -0600

    adding more logic. nothing too substantial or noteworthy

---
+++ Random Characters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwordGenerator
+{
+    internal class Random_Characters
+    {
+        private enum ALPHABET
+        {
+            a,
+            b,
+            c,
+            d,
+            e, f,
+            g, h,
+            i, j,
+            k, l,
+            m, n,
+            o, p, q,
+            r, s, t,
+            u, v,
+            w, x, y, z
+        }
+    }
+
+    //rnd.Next(x, y);  // creates a number between x and y-1
+}
